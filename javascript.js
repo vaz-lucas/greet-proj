7690c3d3541b515ccd0537a85cf094004d27900a
vaz-lucas <lucas.vazxp@gmail.com>
2025-04-04 19:50:30 -0300

    adjusting order and cleaning code

--- javascript.js
+++ javascript.js
@@ -1,37 +1,46 @@
 
-function greet(age) {
-    if(age < 18 ) {
-        return "Wazup, " + age +"er";
-    } else {
-        return "Greetings, " + age +"er";
-    }
-}
 
-function onClick() {
-    const age = parseInt(input.value);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const button = document.querySelector('button')
+    const input = document.getElementById('onlyInput');
+    const resultText = document.querySelector('p');
+
+    function greet(age) {
+        if(age < 18 ) {
+            return "Wazup, " + age +"er";
+        } else {
+            return "Greetings, " + age +"er";
+        }
+    }
     
-    if(isEmpty()) {
-        resultText.textContent = 'Please enter your age';
-    } else if(age < 0) {
-        resultText.textContent = 'Age cannot be negative!';
-    } else {
-        resultText.textContent = greet(age);
+    function onClick() {
+        const age = parseInt(input.value);
+        
+        if(isEmpty()) {
+            resultText.textContent = 'Please enter your age';
+        } else if(age < 0) {
+            resultText.textContent = 'Age cannot be negative!';
+        } else {
+            resultText.textContent = greet(age);
+        }
     }
-}
-
-function isEmpty() {
-    if(input.value === '') {
-        return true;
-    } else {
-        return false
-    }
-};
-
-
+    
+    function isEmpty() {
+        if(input.value === '') {
+            return true;
+        } else {
+            return false
+        }
+    };
+    
 
 
-let button = document.querySelector('button')
-let input = document.getElementById('onlyInput');
-let resultText = document.querySelector('p');
+    button.addEventListener('click', onClick);
+})
 
-button.addEventListener('click', onClick);
+
+
+
+
